5ec5b102951dd47f974db5f412007296a1f3a4f5
Paul Horn <knutwalker@gmail.com>
2014-09-09 00:41:39 +0200

    Slides

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  voltaire = require('bespoke-theme-voltaire'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  state = require('bespoke-state');
+
+// Bespoke.js
+bespoke.from('article', [
+  voltaire(),
+  keys(),
+  touch(),
+  bullets('.bullets>li,.bullet'),
+  scale(),
+  hash(),
+  progress(),
+  state()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
